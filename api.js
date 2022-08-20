d23de53caad44e1939f0a6472115bdf94369104d
Junior <dirceusilva0422@gmail.com>
2022-08-20 11:44:55 -0300

    first commit

---
+++ api.js
@@ -0,0 +1,4 @@
+export const API_KEY = 'b7a46fa1efd1f27fefe086d470b6a7ef';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=en-US';
